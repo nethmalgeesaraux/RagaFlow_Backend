0a07a6501f351e806962b214ec8b143700648120
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-22 19:54:16 +0530

    initial commit

---
+++ src/main/java/com/ng/ragaFlow/RagaFlowApplication.java
@@ -0,0 +1,13 @@
+package com.ng.ragaFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ng/ragaFlow/RagaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.ragaFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
