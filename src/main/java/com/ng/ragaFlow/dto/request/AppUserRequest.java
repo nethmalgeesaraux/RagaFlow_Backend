7a7c476b70265c4cd1bd2df3dc608f162bb51708
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-22 21:47:08 +0530

    create AppUserRequest dto

---
+++ src/main/java/com/ng/ragaFlow/dto/request/AppUserRequest.java
@@ -0,0 +1,31 @@
+package com.ng.ragaFlow.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUserRequest {
+
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String oldPassword;
+
+    @Pattern(regexp = "^(USER|ADMIN)$", message = "Role must be either USER or ADMIN")
+    private String role;
+
+    private String refreshToken;
+
+}
