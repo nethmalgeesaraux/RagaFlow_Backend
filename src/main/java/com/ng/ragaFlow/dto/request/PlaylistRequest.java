9318939dcdb0dfbc4e581609c6833f261af99eae
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-23 20:36:48 +0530

    create PlaylistRequest dto

---
+++ src/main/java/com/ng/ragaFlow/dto/request/PlaylistRequest.java
@@ -0,0 +1,22 @@
+package com.ng.ragaFlow.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaylistRequest {
+
+    @NotBlank(message = "Playlist name is required")
+    @Size(max = 100, message = "Playlist name must not exceed 100 characters")
+    private String name;
+
+    @Size(max = 500, message = "Description must not exceed 500 characters")
+    private String description;
+
+    private Boolean isPublic;
+}
