4ccdd2c95418dc3cea59548286e251554f196bee
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-23 20:45:02 +0530

    create AppUserResponse dto

---
+++ src/main/java/com/ng/ragaFlow/dto/response/AppUserResponse.java
@@ -0,0 +1,21 @@
+package com.ng.ragaFlow.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUserResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
+    private String accessToken;
+    private String refreshToken;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
