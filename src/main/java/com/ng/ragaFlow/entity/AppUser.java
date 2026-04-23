4122c45dedbfaedbd5ce00020174dd02252e7170
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-23 21:42:24 +0530

    AppUser entity

---
+++ src/main/java/com/ng/ragaFlow/entity/AppUser.java
@@ -0,0 +1,47 @@
+package com.ng.ragaFlow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(name = "refresh_token", length = 500)
+    private String refreshToken;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
+
