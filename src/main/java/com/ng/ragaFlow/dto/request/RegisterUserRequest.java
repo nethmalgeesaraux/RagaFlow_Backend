153b7d40158975a7ee859f8ffb6ee0fdeca16af5
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-23 20:39:50 +0530

    create RegisterUserRequest dto

---
+++ src/main/java/com/ng/ragaFlow/dto/request/RegisterUserRequest.java
@@ -0,0 +1,24 @@
+package com.ng.ragaFlow.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterUserRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    private String role;
+}
