8fa2afec46dc41e3fef19d26cadddae5cf76be2a
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-24 13:38:05 +0530

    Playlist entity

---
+++ src/main/java/com/ng/ragaFlow/entity/Playlist.java
@@ -0,0 +1,47 @@
+package com.ng.ragaFlow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "playlist")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "is_public", nullable = false)
+    private Boolean isPublic = false;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appUser_id", nullable = false)
+    private AppUser appUser;
+
+}
