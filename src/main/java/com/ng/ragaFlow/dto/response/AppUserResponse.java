398cf5ddbf436832c5d713cb0cc152bfe866db91
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-25 13:25:34 +0530

    AppResponse Update

--- src/main/java/com/ng/ragaFlow/dto/response/AppUserResponse.java
+++ src/main/java/com/ng/ragaFlow/dto/response/AppUserResponse.java
@@ -1,5 +1,6 @@
 package com.ng.ragaFlow.dto.response;
 
+import com.ng.ragaFlow.entity.AppUser;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,18 @@ public class AppUserResponse {
     private String refreshToken;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public static AppUserResponse fromEntity(AppUser appUser, String accessToken, String refreshToken) {
+        AppUserResponse response = new AppUserResponse();
+        response.setId(appUser.getId());
+        response.setName(appUser.getName());
+        response.setEmail(appUser.getEmail());
+        response.setRole(appUser.getRole());
+        response.setAccessToken(accessToken);
+        response.setRefreshToken(refreshToken);
+        response.setCreatedAt(appUser.getCreatedAt());
+        response.setUpdatedAt(appUser.getUpdatedAt());
+        return response;
+
+    }
 }
