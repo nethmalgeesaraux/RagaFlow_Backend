5a2d1f0f99c7a1caa8e6d2bdaeee5e9d797082e7
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-25 20:19:44 +0530

    SongResponse Update

--- src/main/java/com/ng/ragaFlow/dto/response/SongResponse.java
+++ src/main/java/com/ng/ragaFlow/dto/response/SongResponse.java
@@ -1,5 +1,6 @@
 package com.ng.ragaFlow.dto.response;
 
+import com.ng.ragaFlow.entity.Song;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,20 @@ public class SongResponse {
     private LocalDateTime createdAt;
     private Long appUserId;
     private String appUserName;
+
+    public static SongResponse fromEntity(Song song, String baseUrl) {
+        SongResponse response = new SongResponse();
+        response.setId(song.getId());
+        response.setTitle(song.getTitle());
+        response.setArtist(song.getArtist());
+
+        response.setSongUrl(song.getSongUrl() != null ? baseUrl + song.getSongUrl() : null);
+        response.setImageUrl(song.getImageUrl() != null ? baseUrl + song.getImageUrl() : null);
+
+        response.setCreatedAt(song.getCreatedAt());
+        response.setAppUserId(song.getAppUser().getId());
+        response.setAppUserName(song.getAppUser().getName());
+        return response;
+    }
+
 }
